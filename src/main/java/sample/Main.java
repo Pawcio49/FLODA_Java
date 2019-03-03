e53a6033248577af414be12fb20516b7d006e21e
Paweł <pawel.dopierala4@gmail.com>
2019-03-04 00:11:38 +0100

    Do statistics

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,14 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Objects;
 
 public class Main extends Application {
-
+    @Autowired
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("screen/menu.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("screen/menu.fxml")));
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
